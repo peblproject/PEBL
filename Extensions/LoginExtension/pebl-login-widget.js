67d8223912ecf6fdd5eb809c28eceb7d230a9947
mile.divovic <mile.divovic@eduworks.com>
2018-09-13 15:07:45 -0700

    Fix variable name error

--- Extensions/LoginExtension/pebl-login-widget.js
+++ Extensions/LoginExtension/pebl-login-widget.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     if (window.ReadiumSDK == null) {
 	PEBL.start(false, function(readypebl) {
 	    pebl = readypebl;
-	    if (!PEBL-buttonLogin)
+	    if (!PEBLbuttonLogin)
 		pebl.login(function() {
 		    dosomething();
 		});
@@ -242,7 +242,7 @@ function dosomething() {
 }
 
 function useOpenIDLoginButton(elementName) {
-    PEBL-buttonLogin = true;
+    PEBLbuttonLogin = true;
     
     Lightbox.createLoginButton(elementName);
 }
