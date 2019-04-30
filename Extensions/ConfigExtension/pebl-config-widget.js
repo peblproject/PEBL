e3d08b807c68a612c10a994723a162799842b371
mile.divovic <md2950@nyu.edu>
2019-04-30 11:13:54 -0700

    Update config extension to latest

--- Extensions/ConfigExtension/pebl-config-widget.js
+++ Extensions/ConfigExtension/pebl-config-widget.js
@@ -1,1 +1,28 @@
-var embeddedBookName = 'PEBL_Phishing_in_Cyber_Security';
+var globalPebl = window.top.PeBL;
+
+var config = {};
+
+globalPebl.extension.config = config;
+
+// These functions run when the page is loaded.
+config.onLoadFunctions = {
+
+}
+// Data Entry extension config
+config.dataEntry = {
+    
+}
+// lowStakes Quiz extension config
+config.lowStakesQuiz = {
+	
+}
+
+$(document).ready(function() {
+    if (config.onLoadFunctions) {
+        Object.values(config.onLoadFunctions).forEach(function(value) {
+            if (typeof value === "function") {
+                value();
+            }
+        });
+    }
+});
