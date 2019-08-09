697f9e5ebe2a351b2fc95db9dfb4deff0a71b01a
mile.divovic <md2950@nyu.edu>
2019-08-09 12:20:56 +0200

    fix content morphs not persisting

--- Extensions/ContentMorphingExtension/pebl-contentMorphing-widget.js
+++ Extensions/ContentMorphingExtension/pebl-contentMorphing-widget.js
@@ -1,7 +1,18 @@
 var globalPebl = window.parent.PeBL;
 
+var contentMorphing = {};
+if (globalPebl)
+    globalPebl.extension.contentMorphing = contentMorphing;
+
 $(document).ready(function() {
-	$('.contentMorphing_contentMorphingExtension').each(function() {
+	$('.peblExtension[data-peblextension="contentMorphingControlPanel"]').each(function() {
+		var insertID = this.getAttribute('id');
+		var levels = JSON.parse(this.getAttribute('data-levels'));
+		var id = this.getAttribute('data-id');
+		contentMorphing.createMorphingControlPanel(insertID, levels, id);
+	});
+
+	$('.peblExtension[data-peblextension="contentMorphing"], .contentMorphing_contentMorphingExtension').each(function() {
 		var insertID = $(this)[0].getAttribute('id');
 		var levels = JSON.parse($(this)[0].getAttribute('data-levels'));
 		var levelContent = JSON.parse($(this)[0].getAttribute('data-levelContent'));
@@ -9,12 +20,96 @@ $(document).ready(function() {
 		var cassMapping = $(this)[0].hasAttribute('data-cassMapping') ? $(this)[0].getAttribute('data-cassMapping') : null;
 		var cassTarget = $(this)[0].hasAttribute('data-cassTarget') ? $(this)[0].getAttribute('data-cassTarget') : null;
 		var cassLevels = $(this)[0].hasAttribute('data-cassLevels') ? JSON.parse($(this)[0].getAttribute('data-cassLevels')) : null;
-		createMorphing(insertID, levels, levelContent, id, cassMapping, cassTarget, cassLevels);
+		var controls = this.hasAttribute('data-controls') ? this.getAttribute('data-controls') : 'true';
+		var controller = this.hasAttribute('data-controller') ? this.getAttribute('data-controller') : 'user';
+		contentMorphing.createMorphing(insertID, levels, levelContent, id, cassMapping, cassTarget, cassLevels, controls, controller);
 	});
 });
 
+contentMorphing.createMorphingControlPanel = function(insertID, levels, id) {
+	var controlPanelContainer = document.createElement('div');
+	controlPanelContainer.classList.add('contentMorphingControlPanelContainer');
+
+	var whichLevels = localStorage.getItem('contentMorphingControlPanel-' + id);
+	if (whichLevels)
+		whichLevels = JSON.parse(whichLevels);
+
+	for (var i = 0; i < levels.length; i++) {
+		var levelCheckboxContainer = document.createElement('div');
+		levelCheckboxContainer.classList.add('contentMorphingControlPanelLevelCheckboxContainer');
+
+		var levelCheckbox = document.createElement('input');
+		levelCheckbox.type = 'checkbox';
+		levelCheckbox.value = 'level' + (i + 1) + '-' + levels[i];
+		if (whichLevels) {
+			for (var level of whichLevels) {
+				if (level === levelCheckbox.value)
+					levelCheckbox.checked = true;
+			}
+		} 
+
+		var levelLabel = document.createElement('span');
+		levelLabel.textContent = levels[i];
+
+		levelCheckboxContainer.appendChild(levelCheckbox);
+		levelCheckboxContainer.appendChild(levelLabel);
+
+		controlPanelContainer.appendChild(levelCheckboxContainer);
+	}
+
+	var submitButton = document.createElement('button');
+	submitButton.classList.add('contentMorphingControlPanelSubmitButton');
+	submitButton.textContent = 'Apply';
+	submitButton.addEventListener('click', function() {
+		var checkboxes = $(controlPanelContainer).find('input[type="checkbox"]');
+		var activeLevels = [];
+
+		for (var i = 0; i < checkboxes.length; i++) {
+			if (checkboxes[i].checked) {
+				activeLevels.push(checkboxes[i].value);
+			}
+		}
+
+
+		if (activeLevels.length > 0) {
+			localStorage.setItem('contentMorphingControlPanel-' + id, JSON.stringify(activeLevels));
+		} else {
+			localStorage.removeItem('contentMorphingControlPanel-' + id);
+		}
+
+		contentMorphing.applyMorphingControlPanel(id);
+	});
+
+	controlPanelContainer.appendChild(submitButton);
+
+	insertLocation = document.getElementById(insertID);
+
+    insertLocation.parentNode.insertBefore(controlPanelContainer, insertLocation);
+    insertLocation.remove();
+}
+
+contentMorphing.applyMorphingControlPanel = function(id) {
+	var whichLevels = localStorage.getItem('contentMorphingControlPanel-' + id);
+	var elems = $('#' + id + '.adjustable-content').children();
+
+	for (var elem of elems) {
+		$(elem).hide();
+	}
+
+	if (whichLevels) {
+		whichLevels = JSON.parse(whichLevels);
+		for (var level of whichLevels) {
+			var elem = $('#' + id + '.adjustable-content #' + level).parent();
+			elem.show();
+		}
+		$('#' + id + '.adjustable-content').addClass('active');
+	} else {
+		$('#' + id + '.adjustable-content').removeClass('active');
+	}
+}
+
 //Takes an array with names of desired levels, and array with the content to be contained within those levels, optional parameters cassMapping, cassTarget, cassLevels[]
-function createMorphing(insertID, levels, levelContent, id, cassMapping, cassTarget, cassLevels) {
+contentMorphing.createMorphing = function(insertID, levels, levelContent, id, cassMapping, cassTarget, cassLevels, controls, controller) {
 	var adjustableContentDiv,
 		levelOptionsDiv,
 		currentLevelDiv,
@@ -35,7 +130,7 @@ function createMorphing(insertID, levels, levelContent, id, cassMapping, cassTar
 
 	currentLevelDiv = document.createElement('div');
 	currentLevelDiv.classList.add('current-level', 'set-level1');
-	currentLevelDiv.addEventListener('click', handleCurrentLevelClick);
+	currentLevelDiv.addEventListener('click', contentMorphing.handleCurrentLevelClick);
 
 	levelSelectList = document.createElement('ul');
 
@@ -55,7 +150,7 @@ function createMorphing(insertID, levels, levelContent, id, cassMapping, cassTar
 
 
 		clickEvent = document.createAttribute('onclick');
-		clickEvent.value = "setLevel('" + convertedValue + "', '" + id + "');";
+		clickEvent.value = "globalPebl.extension.contentMorphing.setLevel('" + convertedValue + "', '" + id + "');";
 
 		levelListElement.classList.add('select-level' + convertedValue);
 
@@ -73,7 +168,10 @@ function createMorphing(insertID, levels, levelContent, id, cassMapping, cassTar
 	levelOptionsDiv.appendChild(currentLevelDiv);
 	levelOptionsDiv.appendChild(levelSelectList);
 
-	adjustableContentDiv.appendChild(levelOptionsDiv);
+
+
+	if (controls === 'true')
+		adjustableContentDiv.appendChild(levelOptionsDiv);
 
 	for (content in levelContent) {
 		var convertedValue = parseInt(content) + 1;
@@ -89,25 +187,39 @@ function createMorphing(insertID, levels, levelContent, id, cassMapping, cassTar
 		delete outerHTML;
 	}
 
+
+
     insertLocation = document.getElementById(insertID);
 
     insertLocation.parentNode.insertBefore(adjustableContentDiv, insertLocation);
+
+    if (controller === 'user') {
+		adjustableContentDiv.classList.add('active');
+		var elem = $('#' + id + '.adjustable-content .level1');
+		elem.show();
+	} else if (controller === 'controlPanel') {
+		setTimeout(function() {
+			contentMorphing.applyMorphingControlPanel(id);
+		}, 500);
+	}
+
+
     insertLocation.remove();
 }
 
-function handleCurrentLevelClick() {
+contentMorphing.handleCurrentLevelClick = function() {
     //console.log('Toggle level');
     $(this).next('ul').toggleClass('active');
 }
 
-function isLevelSet(n, sectionID) {
+contentMorphing.isLevelSet = function(n, sectionID) {
     return $("#" + sectionID + ".adjustable-content .level-options .current-level").hasClass('set-level' + n);
 }
 
-function setLevel(n, sectionID, programInvoked) {
+contentMorphing.setLevel = function(n, sectionID, programInvoked) {
     //    $("#"+sectionID+".adjustable-content .level-options .current-level").removeClass('set-level' + contentLevel);
 
-    // if (isLevelSet(n, sectionID))
+    // if (contentMorphing.isLevelSet(n, sectionID))
     //  return;
     
     $("#" + sectionID + ".adjustable-content .level-options .current-level").removeClass('set-level1 set-level2 set-level3');
