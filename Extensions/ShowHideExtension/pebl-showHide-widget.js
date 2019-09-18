7af6617c388c35b530d71584712ed5301d9dde72
mile.divovic <md2950@nyu.edu>
2019-09-18 10:23:07 -0700

    update showhide js

--- Extensions/ShowHideExtension/pebl-showHide-widget.js
+++ Extensions/ShowHideExtension/pebl-showHide-widget.js
@@ -5,7 +5,7 @@ var showHide = {};
 
 globalPebl.extension.showHide = showHide;
 
-showHide.toggleVisibility = function(event, programInvoked) {
+showHide.toggleVisibility = function (event, programInvoked) {
     console.log(event);
     var id = event.currentTarget.getAttribute('otherId'),
         buttonText1 = event.currentTarget.getAttribute('buttonText1'),
@@ -20,34 +20,32 @@ showHide.toggleVisibility = function(event, programInvoked) {
         // $('#' + id + 'Btn').html(res);
         $('#' + id + 'Btn')[0].classList.remove('hiding');
         if (buttonText2 != null) {
-            $('#' + id + 'Btn').text(buttonText2);
-        } else {
-            $('#' + id + 'Btn').children('i').first()[0].classList.remove('fa-plus');
-            $('#' + id + 'Btn').children('i').first()[0].classList.add('fa-minus');
+            $('#' + id + 'Btn').children('span').first().text(buttonText2);
         }
-    state = "showing";
+        $('#' + id + 'Btn').children('i').first()[0].classList.remove('fa-plus-circle');
+        $('#' + id + 'Btn').children('i').first()[0].classList.add('fa-minus-circle');
+        state = "showing";
     } else {
         // var res = str.replace(buttonText2, buttonText1);
         // $('#' + id + 'Btn').html(res);
         $('#' + id + 'Btn')[0].classList.add('hiding');
         if (buttonText1 != null) {
-            $('#' + id + 'Btn').text(buttonText1);
-        } else {
-            $('#' + id + 'Btn').children('i').first()[0].classList.add('fa-plus');
-            $('#' + id + 'Btn').children('i').first()[0].classList.remove('fa-minus');
+            $('#' + id + 'Btn').children('span').first().text(buttonText1);
         }
-    state = "hiding";
+        $('#' + id + 'Btn').children('i').first()[0].classList.add('fa-plus-circle');
+        $('#' + id + 'Btn').children('i').first()[0].classList.remove('fa-minus-circle');
+        state = "hiding";
     }
 
     var currentPage = JSON.parse(globalReadium.reader.bookmarkCurrentPage());
     $('#' + id).slideToggle(400, function () {
         //globalReadium.reader.plugins.highlights.redrawAnnotations();
-        setTimeout(function() {
-            globalReadium.reader.openSpineItemElementCfi(currentPage.idref, currentPage.contentCFI);
-        }, 500);
+        // setTimeout(function () {
+        //     globalReadium.reader.openSpineItemElementCfi(currentPage.idref, currentPage.contentCFI);
+        // }, 500);
     });
 
-    
+
     if (globalPebl != null) {
         if (!programInvoked) {
             var target = document.getElementById(id + 'Btn').dataset.cassTarget;
@@ -56,15 +54,15 @@ showHide.toggleVisibility = function(event, programInvoked) {
             // if (window.top.ReadiumSDK != null)
             //  cfi = window.top.ReadiumSDK.reader.getCfiForElement($("#" + target));
             globalPebl.emitEvent(globalPebl.events.eventPreferred, {
-        		target: target,
-        		type: state
-	        });
+                target: target,
+                type: state
+            });
         }
-    }   
+    }
 }
 
-$(document).ready(function() {
-    $('.showHide_showHideExtension').each(function() {
+$(document).ready(function () {
+    $('.showHide_showHideExtension').each(function () {
         var insertID = $(this)[0].getAttribute('id');
         var buttonText1 = $(this)[0].getAttribute('data-buttonText1');
         var buttonText2 = $(this)[0].getAttribute('data-buttonText2');
@@ -76,11 +74,13 @@ $(document).ready(function() {
         if ($(this)[0].hasAttribute('data-displayBtnInline') && $(this)[0].getAttribute('data-displayBtnInline') == 'true') {
             isInline = true;
         }
-        showHide.createShowHide(insertID, buttonText1, buttonText2, id, isInline,cassMapping, cassTarget, cassLevel);
+
+        var defaultState = this.hasAttribute('data-defaultState') ? this.getAttribute('data-defaultState') : 'closed'
+        showHide.createShowHide(insertID, buttonText1, buttonText2, id, isInline, cassMapping, cassTarget, cassLevel, defaultState);
     });
 });
 
-showHide.createShowHide = function(insertID, buttonText1, buttonText2, id, isInline, cassMapping, cassTarget, cassLevel) {
+showHide.createShowHide = function (insertID, buttonText1, buttonText2, id, isInline, cassMapping, cassTarget, cassLevel, defaultState) {
     var button,
         buttonIcon,
         insertLocation,
@@ -90,25 +90,33 @@ showHide.createShowHide = function(insertID, buttonText1, buttonText2, id, isInl
     button.id = id + 'Btn';
     button.setAttribute('otherId', id);
     button.classList.add('showHideButton');
-    button.classList.add('hiding');
+    if (defaultState === 'closed')
+        button.classList.add('hiding');
     if (isInline == true) {
         button.classList.add('inline');
     }
     button.setAttribute('buttonText1', buttonText1);
     button.setAttribute('buttonText2', buttonText2);
     button.addEventListener('click', showHide.toggleVisibility);
+    buttonIcon = document.createElement('i');
+    buttonIcon.classList.add('fa');
+    if (defaultState === 'closed') {
+        buttonIcon.classList.add('fa-plus-circle');
+    } else {
+        buttonIcon.classList.add('fa-minus-circle');
+    }
+    button.appendChild(buttonIcon);
 
     if (buttonText1 != null && buttonText2 != null) {
+        var buttonText = document.createElement('span');
+        if (defaultState === 'closed') {
+            buttonText.textContent = buttonText2;
+        } else {
+            buttonText.textContent = buttonText1;
+        }
         button.classList.add('text');
-        button.textContent = buttonText1;
+        button.appendChild(buttonText);
     }
-    else {
-        buttonIcon = document.createElement('i');
-        buttonIcon.classList.add('fa', 'fa-plus');
-        button.appendChild(buttonIcon);
-    }
-    
-    
 
     if (cassMapping) {
         button.setAttribute('data-cassMapping', cassMapping);
