840cd4aaee79c28aefa1e2ed7ac8c682a603c862
mile.divovic <md2950@nyu.edu>
2019-12-30 14:05:26 -0800

    update ask expert extension

--- Extensions/AskExpertExtension/pebl-askExpert-widget.js
+++ Extensions/AskExpertExtension/pebl-askExpert-widget.js
@@ -1,8 +1,32 @@
-var globalPebl = window.parent.PeBL;
+var globalPebl = (window.parent && window.parent.PeBL) ? window.parent.PeBL : (window.PeBL ? window.PeBL : null);
 
 var askExpert = {};
 
 globalPebl.extension.askExpert = askExpert;
+
+jQuery(document).ready(function() {
+	jQuery('.peblExtension[data-peblextension="askExpert"]').each(function () {
+        var insertID = this.getAttribute('id');
+        var buttonText = this.getAttribute('data-buttonText');
+        var experts = this.hasAttribute('data-experts') ? JSON.parse(this.getAttribute('data-experts')) : null;
+        var mailto = this.hasAttribute('data-mailto') ? this.getAttribute('data-mailto') : 'false';
+        var mailtoRecipient = this.hasAttribute('data-mailtoRecipient') ? this.getAttribute('data-mailtoRecipient') : null;
+        var mailtoSubject = this.hasAttribute('data-mailtoSubject') ? this.getAttribute('data-mailtoSubject') : null;
+        var mailtoBody = this.hasAttribute('data-mailtoBody') ? this.getAttribute('data-mailtoBody') : null;
+        askExpert.createAskExpertButton(insertID, buttonText, experts, mailto, mailtoRecipient, mailtoSubject, mailtoBody);
+    });
+});
+
+// hide modals when clicking other areas
+jQuery(document).mouseup(function (e) {
+    var container = jQuery('.peblModal');
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        &&
+        container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.remove();
+    }
+});
 
 askExpert.createAskExpert = function(element) {
 		//clearUI();
@@ -26,8 +50,156 @@ askExpert.createAskExpert = function(element) {
 	    var askFrame = document.createElement('iframe');
 	    askFrame.id = 'askFrame';
 	    askFrame.classList.add('askFrame');
-	    askFrame.src = 'https://ask.extension.org/groups/1982/ask';
+	    askFrame.src = '' // TODO: add src;
 	    wrapper.appendChild(askFrame);
 	    askContainer.appendChild(wrapper);
 	    element.appendChild(askContainer);
+}
+
+askExpert.createAskExpertExpertModal = function(id, experts, mailto, mailtoSubject, mailtoBody) {
+	globalPebl.user.getUser(function(userProfile) {
+		if (userProfile) {
+			var expertsModal = document.createElement('div');
+			expertsModal.classList.add('askExpertExpertsModal', 'peblModal');
+			expertsModal.id = 'askExpertExpertsModal';
+
+			var expertsModalHeader = document.createElement('div');
+			expertsModalHeader.classList.add('askExpertExpertsModalHeader');
+
+			var expertsModalHeaderText = document.createElement('span');
+			expertsModalHeaderText.classList.add('askExpertExpertsModalHeaderText');
+			expertsModalHeaderText.textContent = 'Ask an Expert';
+
+			expertsModalHeader.appendChild(expertsModalHeaderText);
+
+			var expertsModalCloseButton = document.createElement('i');
+			expertsModalCloseButton.classList.add('fa', 'fa-times', 'askExpertExpertsModalCloseButton');
+			expertsModalCloseButton.addEventListener('click', function() {
+				jQuery('#askExpertExpertsModal').remove();
+				globalPebl.emitEvent(globalPebl.events.eventUndisplayed, {
+					target: id,
+					type: 'expert'
+				});
+			});
+
+			expertsModalHeader.appendChild(expertsModalCloseButton);
+
+			expertsModal.appendChild(expertsModalHeader);
+
+			var expertsModalBody = document.createElement('div');
+			expertsModalBody.classList.add('askExpertExpertsModalBody');
+
+			expertsModal.appendChild(expertsModalBody);
+
+			var askExpertModalDescription = document.createElement('p');
+			askExpertModalDescription.classList.add('askExpertExpertsModalDescription');
+			askExpertModalDescription.textContent = 'Have questions about the Making Good Instructors Great material? Ask an expert here.';
+
+			expertsModalBody.appendChild(askExpertModalDescription);
+
+			for (var expert of experts) {
+				var expertContainer = document.createElement('div');
+				expertContainer.classList.add('askExpertExpertContainer');
+				expertContainer.setAttribute('detailText', 'Your messages with this expert will not be visible by any other users.');
+				expertContainer.id = userProfile.identity + '_' + expert.id;
+
+				var expertName = document.createElement('span');
+				expertName.classList.add('askExpertExpertName');
+		        var expertContactIcon = document.createElement('i');
+		        expertContactIcon.classList.add('fa', 'fa-envelope', 'askExpertIcon');
+				expertName.textContent = " " + expert.name;
+				expertName.prepend(expertContactIcon);
+
+				expertContainer.appendChild(expertName);
+
+				if (expert.description) {
+					var expertDescription = document.createElement('span');
+					expertDescription.classList.add('askExpertExpertDescription');
+					expertDescription.textContent = expert.description;
+					expertContainer.appendChild(expertDescription);
+				}
+
+				
+				(function(expert) {
+					expertContainer.addEventListener('click', function(evt) {
+						if (mailto && mailto === 'true') {
+							var mailToForm = document.createElement('form');
+							mailToForm.style = 'display:none;';
+							var mailtoLink = 'mailto:';
+							if (expert.email) {
+								mailtoLink += expert.email += '?';
+							}
+							if (mailtoSubject) {
+								mailtoLink += ('subject=' + encodeURI(mailtoSubject));
+							}
+							if (mailtoBody) {
+								mailtoLink += ('&body=' + encodeURI(mailtoBody));
+							}
+							mailToForm.action = mailtoLink;
+							mailToForm.method = 'post';
+							evt.currentTarget.appendChild(mailToForm);
+							mailToForm.submit();
+							evt.currentTarget.removeChild(mailToForm);
+						} else {
+							globalPebl.extension.discussion.handleChatButtonClick(this, 'Message ' + expert.name);
+						}
+
+						globalPebl.emitEvent(globalPebl.events.eventAccessed, {
+							name: expert.name,
+							description: expert.description,
+							type: 'expert',
+							target: id
+						});
+					});
+				})(expert);
+				
+
+				expertsModalBody.appendChild(expertContainer);
+			}
+
+			document.body.appendChild(expertsModal);
+
+			globalPebl.emitEvent(globalPebl.events.eventDisplayed, {
+				target: id,
+				type: 'expert'
+			});
+		}
+	});
+}
+
+askExpert.createAskExpertButton = function(insertID, buttonText, experts, mailto, mailtoRecipient, mailtoSubject, mailtoBody) {
+	var askExpertButton = document.createElement('button');
+	askExpertButton.classList.add('askExpertButton');
+	askExpertButton.textContent = buttonText;
+
+	askExpertButton.addEventListener('click', function(evt) {
+		if (mailto && mailto === 'true') {
+			var mailToForm = document.createElement('form');
+			mailToForm.style = 'display:none;';
+			var mailtoLink = 'mailto:';
+			if (mailtoRecipient) {
+				mailtoLink += mailtoRecipient += '?';
+			}
+			if (mailtoSubject) {
+				mailtoLink += ('subject=' + encodeURI(mailtoSubject));
+			}
+			if (mailtoBody) {
+				mailtoLink += ('&body=' + encodeURI(mailtoBody));
+			}
+			mailToForm.action = mailtoLink;
+			mailToForm.method = 'post';
+			evt.currentTarget.appendChild(mailToForm);
+			mailToForm.submit();
+			evt.currentTarget.removeChild(mailToForm);
+		} else if (experts) {
+			askExpert.createAskExpertExpertModal(insertID, experts, mailto, mailtoSubject, mailtoBody);
+		} else {
+			//
+		}
+	});
+
+	insertLocation = document.getElementById(insertID);
+
+    insertLocation.parentNode.insertBefore(askExpertButton, insertLocation);
+    insertLocation.remove();
 }
