da9f18b30ea5cdce904bd85ccff8c3f4ca7438d8
mile.divovic <md2950@nyu.edu>
2019-12-30 14:12:31 -0800

    update knowledge network extension

--- Extensions/KnowledgeNetworkExtension/pebl-knowledgeNetwork-widget.js
+++ Extensions/KnowledgeNetworkExtension/pebl-knowledgeNetwork-widget.js
@@ -1,4 +1,4 @@
-var globalPebl = window.parent.PeBL;
+var globalPebl = (window.parent && window.parent.PeBL) ? window.parent.PeBL : (window.PeBL ? window.PeBL : null);
 
 var knowledgeNetwork = {};
 
@@ -7,8 +7,131 @@ globalPebl.extension.knowledgeNetwork = knowledgeNetwork;
 knowledgeNetwork.frameIsReady = false;
 knowledgeNetwork.searchTerms = [];
 
+// hide modals when clicking other areas
+jQuery(document).mouseup(function (e) {
+    var container = jQuery('.peblModal');
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        &&
+        container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.remove();
+    }
+});
+
+knowledgeNetwork.createAddResourceModal = function(element) {
+    let existingModal = document.getElementById('knowledgeNetworkAddResourceModal');
+    if (existingModal) {
+        jQuery(existingModal).remove();
+    }
+
+    var addResourceModal = document.createElement('div');
+    addResourceModal.classList.add('knowledgeNetworkAddResourceModal', 'peblModal');
+    addResourceModal.id = 'knowledgeNetworkAddResourceModal';
+
+    var addResourceModalHeader = document.createElement('div');
+    addResourceModalHeader.classList.add('knowledgeNetworkAddResourceModalHeader');
+
+    var addResourceModalHeaderText = document.createElement('span');
+    addResourceModalHeaderText.classList.add('knowledgeNetworkAddResourceModalHeaderText');
+    addResourceModalHeaderText.textContent = 'Add Resource';
+
+    addResourceModalHeader.appendChild(addResourceModalHeaderText);
+
+    var addResourceModalCloseButton = document.createElement('i');
+    addResourceModalCloseButton.classList.add('fa', 'fa-times', 'knowledgeNetworkAddResourceModalCloseButton');
+    addResourceModalCloseButton.addEventListener('click', function() {
+        jQuery('#knowledgeNetworkAddResourceModal').remove();
+    });
+
+    addResourceModalHeader.appendChild(addResourceModalCloseButton);
+
+    addResourceModal.appendChild(addResourceModalHeader);
+
+    var addResourceModalBody = document.createElement('div');
+    addResourceModalBody.classList.add('knowledgeNetworkAddResourceModalBody');
+
+    var addResourceDescription = document.createElement('p');
+    addResourceDescription.classList.add('knowledgeNetworkAddResourceModalDescription');
+    addResourceDescription.textContent = 'Enter a title and URL below to add a resource to your Additional Resources. Your resource will be added to the list based on your current place in the book.';
+
+    var addResourceTitle = document.createElement('div');
+    addResourceTitle.classList.add('AddResources_inputContainer');
+
+    var addResourceTitleInputLabel = document.createElement('label');
+    addResourceTitleInputLabel.for = 'knowledgeNetworkAddResourceTitle';
+    addResourceTitleInputLabel.textContent = 'Resource Title: ';
+
+    var addResourceTitleInput = document.createElement('input');
+    addResourceTitleInput.id = 'knowledgeNetworkAddResourceTitle';
+
+    addResourceTitle.appendChild(addResourceTitleInputLabel);
+    addResourceTitle.appendChild(addResourceTitleInput);
+
+    var addResourceUrl = document.createElement('div');
+    addResourceUrl.classList.add('AddResources_inputContainer');
+
+    var addResourceUrlInputLabel = document.createElement('label');
+    addResourceUrlInputLabel.for = 'knowledgeNetworkAddResourceUrl';
+    addResourceUrlInputLabel.textContent = 'Resource URL: ';
+
+    var addResourceUrlInput = document.createElement('input');
+    addResourceUrlInput.id = 'knowledgeNetworkAddResourceUrl'
+
+    addResourceUrl.appendChild(addResourceUrlInputLabel);
+    addResourceUrl.appendChild(addResourceUrlInput);
+
+    var addResourceSuccess = document.createElement('div');
+    addResourceSuccess.classList.add('knowledgeNetworkAddResourceModalSuccess');
+
+    var addResourceSuccessText = document.createElement('p');
+    addResourceSuccessText.textContent = 'Your resource will be added to your Additional Resources shortly.';
+
+    var addResourceAgainButton = document.createElement('button');
+    addResourceAgainButton.textContent = 'Add Another Resource';
+    addResourceAgainButton.addEventListener('click', function () {
+        addResourceUrlInput.value = '';
+        addResourceTitleInput.value = '';
+        jQuery(addResourceSuccess).hide();
+        jQuery(addResourceModalBody).show();
+    });
+
+    addResourceSuccess.appendChild(addResourceSuccessText);
+    addResourceSuccess.appendChild(addResourceAgainButton);
+
+    var addResourceSubmitButton = document.createElement('button');
+    addResourceSubmitButton.textContent = 'Add Resource';
+    addResourceSubmitButton.addEventListener('click', function() {
+        globalPebl.user.getUser(function (userProfile) {
+            var url = addResourceUrlInput.value;
+            var title = addResourceTitleInput.value;
+
+            if (url.trim().length > 0 && title.trim().length > 0 && userProfile) {
+                if (url.substring(0, 8).toLowerCase() !== 'https://') {
+                    window.alert('URL must begin with https://');
+                } else {
+                    knowledgeNetwork.pullResource(userProfile.identity, url, 'html', title, url);
+                    jQuery(addResourceSuccess).show();
+                    jQuery(addResourceModalBody).hide();
+                }
+            } else {
+                window.alert('Please fill out both fields.');
+            }
+        });
+    });
+
+    addResourceModalBody.appendChild(addResourceDescription);
+    addResourceModalBody.appendChild(addResourceTitle);
+    addResourceModalBody.appendChild(addResourceUrl);
+    addResourceModalBody.appendChild(addResourceSubmitButton);
+
+    addResourceModal.appendChild(addResourceModalBody);
+    addResourceModal.appendChild(addResourceSuccess);
+
+    element.appendChild(addResourceModal);
+}
+
 knowledgeNetwork.receiveMessage = function(event) {
-	var data = event.data;
+    var data = event.data;
     var obj;
     try {
         obj = JSON.parse(data);
@@ -71,7 +194,7 @@ knowledgeNetwork.getCurrentPrefix = function(callback) {
 }
 
 knowledgeNetwork.searchType = function(type) {
-	var waitingForReady = setInterval(function() {
+    var waitingForReady = setInterval(function() {
         if (knowledgeNetwork.frameIsReady) {
             clearInterval(waitingForReady);
             var iframe = document.getElementById('registryFrame');
@@ -87,8 +210,8 @@ knowledgeNetwork.searchType = function(type) {
 }
 
 knowledgeNetwork.createRegistrySearch = function(element) {
-	knowledgeNetwork.frameIsReady = false;
-	var currentPage = window.location.pathname.split('/').pop().slice(0, -6).substring(5);
+    knowledgeNetwork.frameIsReady = false;
+    var currentPage = window.location.pathname.split('/').pop().slice(0, -6).substring(5);
     //clearUI();
     //Create the registrySearch page
     var registryContainer = document.createElement('div');
@@ -96,10 +219,10 @@ knowledgeNetwork.createRegistrySearch = function(element) {
     registryContainer.classList.add('registryContainer');
     registryContainer.setAttribute('style', 'overflow: auto;');
 
-   	var closeButton = document.createElement('i');
+       var closeButton = document.createElement('i');
     closeButton.classList.add('fa', 'fa-times', 'registryCloseButton');
     closeButton.addEventListener('click', function() {
-    	$('#registryContainer').remove();
+        $('#registryContainer').remove();
     });
 
     var wrapper = document.createElement('div');
@@ -121,19 +244,19 @@ knowledgeNetwork.createRegistrySearch = function(element) {
     //Post Messages
     var waitingForReady = setInterval(function() {
         if (knowledgeNetwork.frameIsReady) {
-        	globalPebl.user.getUser(function(user) {
-        		clearInterval(waitingForReady);
-	            var iframe = document.getElementById('registryFrame');
-	            var currentUser = user.identity;
-	            var obj = {
-	                "messageType": "Login",
-	                "user": currentUser,
-	                "currentPage": currentPage
-	            }
-	            var message = JSON.stringify(obj);
-	            iframe.contentWindow.postMessage(message, '*');
-	            //Script on registry page would login as that user
-        	});
+            globalPebl.user.getUser(function(user) {
+                clearInterval(waitingForReady);
+                var iframe = document.getElementById('registryFrame');
+                var currentUser = user.identity;
+                var obj = {
+                    "messageType": "Login",
+                    "user": currentUser,
+                    "currentPage": currentPage
+                }
+                var message = JSON.stringify(obj);
+                iframe.contentWindow.postMessage(message, '*');
+                //Script on registry page would login as that user
+            });
         }
     }, 500);
 
